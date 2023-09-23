b3421ef96f5facec3addf10ce3b20b67b4eaf6a5
Sammie Rosado <sammierosado@gmail.com>
2023-09-23 15:54:27 -0500

    added blog page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import AboutPage from './components/AboutPage';
+import Blog from './components/Blog';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
         
         <Route path="/" element={<LandingPage />} />
         <Route path="/about" element={<AboutPage />} />
-        
+        <Route path="/blog" element={<Blog />} />
 
       </Routes>
     </Router>
