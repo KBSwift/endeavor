0fab760b814032aaad75ebe7aaa3d3023a40f994
Angela Mitchell <angela.mitchell127@t-mobile.com>
2023-09-24 16:43:29 -0500

    Make modal work

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,39 @@
-import { Link } from "react-router-dom";
-
 const Card = ({post}) => {
   return (
-    <article>
-      <Link to={`/blog-posts/detail/${post.id}`}>
+    <>
+      <article data-bs-toggle="modal" data-bs-target={`#postModal${post.id}`}>
         <h2>{post.title}</h2>
         <img src={post.image} alt={post.altText} />
-      </Link>
-    </article>
+      </article>
+      <div 
+        className="modal fade"
+        id={`postModal${post.id}`}
+        tabIndex="-1"
+        data-bs-backdrop="static"
+        data-bs-keyboard="false"
+        aria-labelledby="modal-title"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="modal-title">{post.title}</h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              >
+              </button>
+            </div>
+            <div className="modal-body">
+              <img src={post.image} alt={post.altText} className="modal-image card-img-top" />
+              <p>{post.description}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
