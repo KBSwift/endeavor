c208d4ee2e62077aeda2b46167e22e42477d7dd6
Angela Mitchell <angela.mitchell127@t-mobile.com>
2023-09-24 09:56:43 -0500

    Add description

--- src/contexts/Contexts.jsx
+++ src/contexts/Contexts.jsx
@@ -10,17 +10,20 @@ export const PostsContext = createContext([
     title: 'The Impact of Artificial Intelligence on Healthcare',
     image: artificialIntelligence,
     altText: 'Artificial Intelligence',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
   },
   {
     id: 1,
     title: 'Augmented Reality in Education: Transforming Learning Experiences',
     image: augmentedReality,
     altText: 'Augmented Reality',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
   },
   {
     id: 2,
     title: 'Art and Technology: The Intersection of Creativity and Innovation',
     image: digitalArt,
     altText: 'Girl drawing digital art',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
   }
 ]);
