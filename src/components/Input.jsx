b2219d4ce4addb188c86feefa6c4af19cac26740
Elizabeth Beckerle <elzbthyorke@gmail.com>
2023-09-23 19:34:57 -0500

    bootstrap styling for input file type in create content form

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,13 +1,21 @@
 import styles from './Input.module.css';
 
 const Input = ({label, type}) => {
+    if(type === "file"){
+        return(
+            <div class="mb-3">
+                    <label for="formFileMultiple" class="form-label">{label}</label>
+                <input class="form-control" type="file" id="formFileMultiple" multiple></input>
+            </div>
+        );
+    }
     return (
        <> 
             <label>
                 {label}
                 <input type={type} id={label}></input>
             </label><br></br>
-            
+
        </>
     );
   }
