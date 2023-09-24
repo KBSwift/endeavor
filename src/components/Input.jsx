94dd93a50782895b809df0fcb584ffe953d24275
Kavin Moreno <kavinmoreno@gmail.com>
2023-09-24 05:03:42 -0500

    cleaned formatting for input component, re-sorted navigation

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,8 @@
 import styles from './Input.module.css';
 
-const Input = ({label, type}) => {
-    if(type === "file"){
-        return(
+const Input = ({ label, type }) => {
+    if (type === "file") {
+        return (
             <div className="mb-3">
                 <label className="form-label">
                     {label}
@@ -12,14 +12,14 @@ const Input = ({label, type}) => {
         );
     }
     return (
-       <> 
+        <>
             <label>
                 {label}
                 <input type={type} id={label}></input>
             </label><br></br>
 
-       </>
+        </>
     );
-  }
-  
-  export default Input;
+}
+
+export default Input;
